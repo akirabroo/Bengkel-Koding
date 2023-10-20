64c839aa59fb1fb9f11860be9a3913b6d5eaa60d
akirabroo <92978578+akirabroo@users.noreply.github.com>
2023-10-20 09:32:15 +0700

    Add files via upload

---
+++ kegiatan/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'kegiatan';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect(
+    $databaseHost,
+    $databaseUsername,
+    $databasePassword,
+    $databaseName
+);
